0940c64c5213064023993b86741d2bbc77105934
UCaglarCelik <128084645+UCaglarCelik@users.noreply.github.com>
2023-03-17 20:34:55 +0300

    Add files via upload

---
+++ Homework1.py
@@ -0,0 +1,23 @@
+#Veri Tipleri
+
+#int - float - double - complex farklı türdeki sayı değerlerini temsil etmek için kullanılıyor.
+#boolean veri tipi doğru yanlış çıktısı veriyor ve mantık işlemlerinin sonuçlarını temsil ediyor.
+#String veri tipi metinsel değerleri temsil etmek için kullanıyoruz.
+#List veri tipi birden fazla değişkeni içine sokabildiğimiz bir kümeyi temsil ediyor. Bir nevi liste.
+#Tuple veri tipi listeye benziyor ancak içindeki değerlerin veya değişkenlerin sayısı değiştirilemiyor.
+#Dictionary veri tipi bazı değerleri anahtar kelimelere tanımlayarak kullanabilmemize olanak sağlar.
+
+#Kodlama.io Sitesindeki Veriler
+
+#Tüm yazılar - String veri tipi, örnek: String str = Kurs Programı
+#Tüm sayılar - sayısal veri tipleri, örnek: int tamamlanmaYüzdesi = 0
+
+#Kodlama.io Sitesindeki Şart Bloku Kullanılan Yerler
+
+#Ödevlerin tamamlanmasına bağlı olarak tamamlanma yüzdesinin değişmesi
+
+odev1 = False
+tamamlanmaYuzdesi = 0
+
+if odev1 == True:
+    tamamlanmaYuzdesi = tamamlanmaYuzdesi + 10
